feea6b5c1bbe72b93a7846d9fa0ebfe3fd0caf3a
Jane Yu <janeyu@JanedeMacBook-Pro-4.local>
2026-07-06 23:19:12 +0700

    Apply Astryx-inspired UI refresh

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "parcel-scan-cloud-v6";
+const CACHE_NAME = "parcel-scan-cloud-v7";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=11",
-  "./app.js?v=11",
-  "./manifest.webmanifest?v=11",
+  "./styles.css?v=12",
+  "./app.js?v=12",
+  "./manifest.webmanifest?v=12",
   "./icon.svg",
 ];
 
